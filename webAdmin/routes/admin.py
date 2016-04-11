9d12306c9357ab6f02acb83d5034297a1a32b3a2
AliGhahraei <aligf94@gmail.com>
2016-04-11 11:10:27 -0500

    Add working 'conductores in admin'
    This adds 'conductores' to the application. Only tests are missing in the web
    admin.

--- webAdmin/routes/admin.py
+++ webAdmin/routes/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django import forms
 
-from .models import Ruta
+from .models import Ruta, Conductor
 
 class CustomAdmin(admin.AdminSite):
     site_title  = 'Expreso Tec App'
     site_header = 'Expreso Tec App'
     index_title = 'Administración del sitio'
 
+class ConductorForm(forms.ModelForm):
+    class Meta:
+        model=Conductor
+        fields = ['nombre','clave']
+        widgets = {
+            'clave': forms.PasswordInput(),
+        }
+        
+class ConductorAdmin(admin.ModelAdmin):
+    form=ConductorForm
+
 admin_site = CustomAdmin(name='customAdmin')
 admin_site.register(Ruta)
+admin_site.register(Conductor,ConductorAdmin)
 
 # Register your models here.
--- webAdmin/routes/models.py
+++ webAdmin/routes/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.forms import PasswordInput
 
 # Create your models here.
 
+class Conductor(models.Model):
+    nombre=models.CharField(max_length=80)
+    clave=models.CharField(max_length=50,verbose_name='nueva clase')
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = 'conductores'
+
 class Ruta(models.Model):
     nombre=models.CharField(max_length=40)
-    conductor=models.CharField(max_length=80)
+    conductor=models.ForeignKey(Conductor)
     pagina=models.URLField(max_length=100)
     def __str__(self):
         return self.nombre
