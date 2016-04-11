ffd583dfcd74885328234ef6e91554a3f4bf2fcc
AliGhahraei <aligf94@gmail.com>
2016-04-10 20:34:35 -0500

    Use custom admin in urls.py instead of stock

--- webAdmin/webAdmin/urls.py
+++ webAdmin/webAdmin/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.conf.urls import url
+from routes.admin import admin_site
 
 urlpatterns = [
-   # url(r'^login/'  ,login,{'template_name':'login/login.html'},name='login'),
-   # url(r'^logout/' ,logout, name='logout'),
-    url(r'^'        ,admin.site.urls),
+    url(r'^', admin_site.urls),
 ]
