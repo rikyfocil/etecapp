fbcb9b0c4df1c32921edf3f209b6118d9460f166
AliGhahraei <aligf94@gmail.com>
2016-04-16 22:56:24 -0500

    Add configuration required by Heroku
    This commit adds some needed configuration following the advice of:
    https://devcenter.heroku.com/articles/django-app-configuration.

--- webAdmin/webAdmin/settings.py
+++ webAdmin/webAdmin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,6 +116,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Set up our database for Heroku
+dbEnv = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(dbEnv)
+
+# Set up file serving for Heroku
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
